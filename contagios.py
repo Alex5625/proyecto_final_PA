e158c4e7b771dc4555fe3641f00f2c2d4a352717
Alex_5625 <ahernandez23@alumnos.utalca.cl>
2024-07-07 15:17:12 -0400

    Se crea el archivo contagios para albergar la funcion de probabilidad de los contagios

---
+++ contagios.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def contagios(infectado, susceptible):
+
+	infeccion_familia = 0.5
+	print(f"Id del infectado ~~{infectado.id_familia} ~~ Id del supuesto pariente ~~{susceptible.id_familia}~~")
+	#primero ver si las personas son parientes o no, pq si lo son significa que podrian habitar la misma casa y eso significa que 
+	#la probabilidad de contagio sea mayor
+	if infectado.id_familia == susceptible.id_familia: 
+		print("ERES DE LA FAMILIA TE vas a contagiar con la probabiliad de que eres familiar")
+		if np.random.random() <= infeccion_familia:
+			return True
+		else:
+			return False
+	else:
+		print("no eres de la FAMILIA, la probabilidad con la que te van a contagiar sera menor")
+		
+		if np.random.random() <= infectado.enfermedad.__prob_infeccion:
+			return True
+		else:
+			return False
